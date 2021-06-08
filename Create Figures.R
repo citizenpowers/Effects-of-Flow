6be0f59823f7533e6681e078cf7f49e684590191
Matt Powers <citizenpowers@hotmail.com>
2021-06-07 20:54:53 -0400

    Added TP vs Flow for G302 and S5A Added PP vs Flow for G302

--- Create Figures.R
+++ Create Figures.R
@@ -17,7 +17,12 @@ library(e1071)
 
 
 
-ggplot(S5A_WQ_and_flow,aes(Flow,`PHOSPHATE, TOTAL AS P`))+geom_point()+theme_bw()
+ggplot(filter(S5A_WQ_and_flow,Flow>1),aes(Flow,`PHOSPHATE, TOTAL AS P`*1000))+geom_point()+theme_bw()+
+stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~"),color="red"),parse = TRUE)+
+labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Flow at S5A from Grab Samples from Jan 2015 to May 2021",caption="Flows less than 1 cfs excluded")
+
+ggsave("Total Phosphorus vs Flow at S5A from Grab Samples from Jan 2015 to May 2021.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
+
 
 ggplot(S5A_WQ_and_flow,aes(Flow,`PHOSPHATE, ORTHO AS P`))+geom_point()
 
@@ -30,3 +35,14 @@ ggplot(S319_WQ_and_flow,aes(Flow,`PHOSPHATE, ORTHO AS P`))+geom_point()
 
 ggplot(S319_WQ_and_flow,aes(Flow,`PHOSPHATE, DISSOLVED AS P`))+geom_point()
 
+ggplot(filter(G300_G301_G302_WQ_and_flow,Station=="G302",Flow>1),aes(Flow,`PHOSPHATE, TOTAL AS P`*1000))+geom_point()+theme_bw()+
+stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~"),color="red"),parse = TRUE)+  
+labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Flow at G302 from Grab Samples from Jan 2015 to May 2021",caption="Flows less than 1 cfs excluded")
+
+ggsave("Total Phosphorus vs Flow at G302 from Grab Samples from Jan 2015 to May 2021.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
+
+ggplot(filter(G300_G301_G302_WQ_and_flow,Station=="G302",Flow>1),aes(Flow,`Particulate Phosphorus`*1000))+geom_point()+theme_bw()+
+stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~"),color="red"),parse = TRUE)+
+labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Particulate Phosphorus vs Flow at G302 from Grab Samples from Jan 2015 to May 2021",caption="Flows less than 1 cfs excluded")
+
+ggsave("Particulate Phosphorus vs Flow at G302 from Grab Samples from Jan 2015 to May 2021.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
--- Import and Tidy.R
+++ Import and Tidy.R
@@ -19,38 +19,66 @@ S5A_Flow <-  read_csv("Data/S5A Flow 2015-16.csv") %>%
 bind_rows(read_csv("Data/S5A Flow 2017-21.csv")) %>%
 mutate(Date=mdy_hm(`Date Time`))
        
-#Combined Outflow over entire flowway
+#Combined 
 S5A_Flow_by_minute <-  setNames(as.data.frame(seq(from=ISOdate(2015,1,01,0,0,0,tz = "UTC"), to=ISOdate(2021,06,01,0,0,0,tz = "UTC"),by = "min")),"Date") %>%
 left_join(S5A_Flow,by="Date") %>%
 fill(Flow)  
 
-
+#S319
 S319_Flow <-   read_csv("Data/S319 Flow 2015-21.csv") %>%
 mutate(Date=mdy_hm(`Date Time`))
 
-#Combined Outflow over entire flowway
+#Combined O
 S319_Flow_by_minute <-  setNames(as.data.frame(seq(from=ISOdate(2015,1,01,0,0,0,tz = "UTC"), to=ISOdate(2021,06,01,0,0,0,tz = "UTC"),by = "min")),"Date") %>%
 left_join(S319_Flow,by="Date") %>%
 fill(Flow)  
 
+#G300 Flow
+G300_Flow_by_minute <-  setNames(as.data.frame(seq(from=ISOdate(2015,1,01,0,0,0,tz = "UTC"), to=ISOdate(2021,06,01,0,0,0,tz = "UTC"),by = "min")),"Date") %>%
+left_join(mutate(read_csv("Data/G300 Flow 2015-21.csv"),Date=mdy_hm(`Date`)) ,by="Date") %>%
+mutate(Station="G300") %>%  
+fill(Flow)  
 
+#G301 Flow
+G301_Flow_by_minute <-  setNames(as.data.frame(seq(from=ISOdate(2015,1,01,0,0,0,tz = "UTC"), to=ISOdate(2021,06,01,0,0,0,tz = "UTC"),by = "min")),"Date") %>%
+left_join(mutate(read_csv("Data/G301 Flow 2015-21.csv"),Date=mdy_hm(`Date`)) ,by="Date") %>%
+mutate(Station="G301") %>%    
+fill(Flow)  
+
+#G302 Flow
+G302_Flow_by_minute <-  setNames(as.data.frame(seq(from=ISOdate(2015,1,01,0,0,0,tz = "UTC"), to=ISOdate(2021,06,01,0,0,0,tz = "UTC"),by = "min")),"Date") %>%
+left_join(mutate(read_csv("Data/G302 Flow 2015-21.csv"),Date=mdy_hm(`Date`)) ,by="Date") %>%
+mutate(Station="G302") %>%  
+fill(Flow)  
+
+#G300, 301, 302 flow combined
+G300s <- bind_rows(G300_Flow_by_minute,G301_Flow_by_minute) %>%
+bind_rows(G302_Flow_by_minute)   
 
 # Step 2: Import and Tidy WQ data  --------------------------------------
 
-#RPA data from outflows 
+#S5A data 
 S5A_WQ_Data <- read_csv("Data/S5A WQ Data.csv") %>%
 select(Collection_Date,`Test Name`,Value) %>%
 filter(!is.na(`Test Name`))%>%
 pivot_wider(names_from=`Test Name`,values_from=Value) %>%
 mutate(Date=mdy_hm(`Collection_Date`))
 
-#RPA data from outflows 
+#S319 data  
 S319_WQ_Data <- read_csv("Data/S319 WQ Data.csv") %>%
 select(Collection_Date,`Test Name`,Value) %>%
 filter(!is.na(`Test Name`))%>%
 pivot_wider(names_from=`Test Name`,values_from=Value) %>%
 mutate(Date=mdy_hm(`Collection_Date`))
 
+#G300 structures data  
+G302_G301_and_G300_WQ_Data <- read_csv("Data/G302, G301, and G300 WQ Data.csv") %>%
+filter(`Sample Type New`=="SAMP",is.na(Flag)) %>% 
+select(Collection_Date,`Test Name`,Value,`Station ID`) %>%
+filter(!is.na(`Test Name`)) %>%
+pivot_wider(names_from=`Test Name`,values_from=Value) %>%
+mutate(Date=mdy_hm(`Collection_Date`),Station=`Station ID`,`Particulate Phosphorus`=`PHOSPHATE, TOTAL AS P`-`PHOSPHATE, DISSOLVED AS P`) %>%
+select(`PHOSPHATE, TOTAL AS P`,`PHOSPHATE, ORTHO AS P`,`PHOSPHATE, DISSOLVED AS P`,`Particulate Phosphorus`,`Date`,`Station`)
 
 
 # Step 3: Join Flow and RPA data and save DF --------------------------------------
@@ -58,10 +86,11 @@ mutate(Date=mdy_hm(`Collection_Date`))
 S5A_WQ_and_flow <-S5A_WQ_Data %>%
 left_join(S5A_Flow_by_minute,by="Date")
 
-
 S319_WQ_and_flow <-S319_WQ_Data %>%
 left_join(S319_Flow_by_minute ,by="Date")
 
+G300_G301_G302_WQ_and_flow <-G302_G301_and_G300_WQ_Data %>%
+left_join(G300s ,by=c("Date","Station")) 
 
 
 
