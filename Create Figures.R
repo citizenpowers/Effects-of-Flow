da8af0ed2c100abc0b2df21769eed07066e01f5e
citizenpowers <citizenpowers@hotmail.com>
2022-08-14 21:25:33 -0400

    Created markdown report to explore relationship between TP and TN and Flow

--- Create Figures.R
+++ Create Figures.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 
+library(cowplot)
 library(readr)
 library(readxl)
 library(scales)
@@ -11,8 +12,9 @@ library(tidyr)
 library(ggpmisc)
 library(ggthemr)
 library(broom)
-library(memisc)
 library(pander)
+library(nlstools)
+library(ggridges)
 
 # Set theme ---------------------------------------------------------------
 
@@ -49,6 +51,14 @@ mutate(across(where(is.numeric), round, digits=0))
 
 Summary_stats <-  rbind(G538_summary_stats,S5A_summary_stats)
 
+All_flow <-G538_Daily_Average_flow %>% mutate(Station="G538") %>% bind_rows(S5A_Daily_Average_flow %>% mutate(Station="S5A")) %>%
+mutate(Month=month(Day, label = TRUE, abbr =TRUE),Year=year(Day))
+
+ggplot(All_flow, aes(x =Month , y =`Daily Flow`,fill=Station)) +geom_boxplot(position="dodge",outlier.shape = 21)+
+labs(title = 'Mean Daily Flow 2015-22 at S5A and G538 ',y="Mean Daily Flow (cfs)")
+
+ggsave("Mean Daily Flow 2015-22 at S5A and G538.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
+
 
 # Water Quality Summary Stats  --------------------------------------------
 
@@ -75,17 +85,74 @@ mutate(across(where(is.numeric), round, digits=3))
 Summary_stats_WQ <- rbind(G538_WQ_summary_stats_TP,S5A_WQ_summary_stats_TP) %>% rbind(G538_WQ_summary_stats_TN) %>% rbind(S5A_WQ_summary_stats_TN) 
 
 
+# Time Series Plots -------------------------------------------------------
+
+#Create combined DF of S5A and G538
+WQ_Time_Series <- mutate(select(S5A_WQ_and_flow,Date,Flow,`PHOSPHATE, TOTAL AS P`,`TOTAL NITROGEN`),Station="S5A") %>%
+  rbind(mutate(select(G538_WQ_and_flow,Date,Flow,`PHOSPHATE, TOTAL AS P`,`TOTAL NITROGEN`),Station="G538") ) %>%
+  mutate(Year=year(Date),Month=month(Date,label = TRUE,abbr = TRUE),yday=yday(Date),mday=mday(Date)) %>%
+  mutate(datetime =make_datetime(2020, Month, mday, 12,0, 0))  %>%
+  pivot_longer(names_to="Parameter",values_to="Value",3:4)
+
+#Seasonal WQ at G538 and S5A
+ggplot(WQ_Time_Series,aes(datetime,Value,color=as.factor(Station)))+geom_point()+theme_bw()+geom_smooth(fill="grey")+facet_wrap(~Parameter,ncol=1,scales="free_y")+
+  labs(y= expression(mg~L^-1),x="Date", title = "Seasonal WQ Pattern at G538 and S5A")+
+  scale_x_datetime(breaks =make_datetime(2020,1:12), labels = month.abb) 
+
+ggsave("Seasonal WQ Pattern.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 8, units = "in", dpi = 300, limitsize = TRUE)
+
+#Seasonal plots
+ggplot(S5A_WQ_and_flow,aes(Date,`PHOSPHATE, TOTAL AS P`*1000))+geom_point()+theme_bw()+geom_smooth(method="loess",span=.2)+
+  geom_line(data=S5A_Flow,aes(Date,Flow/10),color="red")+  scale_y_continuous( sec.axis = sec_axis(~.*10, name = "Flow (CFS)"))+
+  scale_x_datetime(date_breaks = "3 months",date_labels = "%b%y")+
+  labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Flow at S5A from Grab Samples from Jan 2015 to August 2022")
+
+ggplot(mutate(S5A_WQ_and_flow,`Flow Category`=ifelse(Flow>0,"Flow","No Flow")),aes(month(Date,label = TRUE),`PHOSPHATE, TOTAL AS P`))+
+  geom_jitter(color="light blue")+geom_boxplot(alpha=.5)+theme_bw()+facet_wrap(~`Flow Category`)
+
+
+#Create DF to plot daily average flow 
+G538_Daily_Average_flow_year_month<-G538_Daily_Average_flow %>% 
+  mutate(Year=year(Day),Month=month(Day,label = TRUE,abbr = TRUE),yday=yday(Day),mday=mday(Day)) %>%
+  mutate(datetime =make_datetime(2020, Month, mday, 12,0, 0))  
+
+#Seasonal flow at G538
+ggplot(G538_Daily_Average_flow_year_month,aes(datetime,`Daily Flow`,color=as.factor(Year)))+geom_point()+geom_line()+theme_bw()+
+  labs(y= "Mean Daily Flow (cfs)",x="Date", title = "G538 Seasonal flow Pattern")+
+  scale_x_datetime(breaks =make_datetime(2020,1:12), labels = month.abb) 
+
+ggsave("G538 Seasonal flow Pattern.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
+
+
+
 # Scatter Plots ----------------------------------------------------------
 
 #S5A TPO4
 ggplot(filter(WQ_Time_Series,Flow>1),aes(Flow,Value,color=Station))+geom_point()+theme_bw()+geom_smooth(method="lm",fill="grey")+facet_wrap(~Parameter,ncol=1,scales="free")+
 stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),parse = TRUE)+
-labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Water Quality Analytes vs Flow at G538 and S5A",caption="Flows less than 1 cfs excluded")
+geom_line(data=augment(S5A_model_growth,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1)),aes(Flow,.fitted))+
+labs(y= expression(mg~L^-1),x="Flow (cfs)", title = "Water Quality Analytes vs Flow at G538 and S5A",caption="Flows less than 1 cfs excluded")
+
+#S5A TPO4
+p1 <-ggplot(filter(WQ_Time_Series,Flow>1,Parameter=="PHOSPHATE, TOTAL AS P"),aes(Flow,Value,color=Station))+geom_point()+theme_bw()+geom_smooth(method="lm",fill="grey")+
+stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),parse = TRUE)+
+geom_line(data=augment(S5A_model_growth,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1)),aes(Flow,.fitted),color="black")+
+labs(y= expression(mg~L^-1))+theme(plot.margin = unit(c(1,0.5,0.5,0.5), "cm"),legend.position="none") 
+
+#S5A TN
+p2 <-ggplot(filter(WQ_Time_Series,Flow>1,Parameter=="TOTAL NITROGEN"),aes(Flow,Value,color=Station))+geom_point()+theme_bw()+geom_smooth(method="lm",fill="grey")+
+stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),parse = TRUE)+
+labs(y= expression(mg~L^-1),x="Flow (cfs)",caption="Flows less than 1 cfs excluded. Logistic Growth Model in Black")+
+theme(plot.margin = unit(c(1,0.5,0.5,0.5), "cm"),legend.position="bottom",axis.title.x = element_blank()) 
+
+
+
+plot_grid(p1, p2, labels = c('TPO4 vs Flow', 'TN vs Flow'), label_size = 12,ncol = 1,  hjust = -0.5,vjust =2)
 
 ggsave("Water Quality Analytes vs Flow at G538 and S5A.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 8, units = "in", dpi = 300, limitsize = TRUE)
 
 #S5A Total nitrogen
-ggplot(filter(S5A_WQ_and_flow,Flow>-1000),aes(Flow,`TOTAL NITROGEN`))+geom_point()+theme_bw()+geom_smooth(method="lm")+
+ggplot(filter(S5A_WQ_and_flow,Flow>1),aes(Flow,`TOTAL NITROGEN`))+geom_point()+theme_bw()+geom_smooth(method="lm")+
 stat_poly_eq(formula = y~x, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~"),color="red"),parse = TRUE)+scale_x_continuous(breaks=c(seq(0,6400,800)))+
 labs(y= expression(TN~mg~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Flow at S5A from Grab Samples from Jan 2015 to Aug 2022",caption="Flows less than 1 cfs excluded")
 
@@ -111,61 +178,65 @@ labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Fl
 
 # Model Plots -------------------------------------------------------------
 
+#create models TP
 S5A_model_greater_than_one <- lm(`PHOSPHATE, TOTAL AS P` ~ Flow, data = filter(S5A_WQ_and_flow,Flow>1))
 S5A_model <- lm(`PHOSPHATE, TOTAL AS P` ~ Flow, data = S5A_WQ_and_flow)
-
+S5A_model_growth = nls(`PHOSPHATE, TOTAL AS P`~ SSlogis(Flow, a,b , c), data = filter(S5A_WQ_and_flow,Flow>1))
 G538_model <- lm(`PHOSPHATE, TOTAL AS P` ~ Flow, data = G538_WQ_and_flow)
+G538_model_greater_than_one <- lm(`PHOSPHATE, TOTAL AS P` ~ Flow, data = filter(G538_WQ_and_flow,Flow>1))
 
-summary(S5A_model)
+#create models TN
+S5A_model_greater_than_one_TN <- lm(`TOTAL NITROGEN` ~ Flow, data = filter(S5A_WQ_and_flow,Flow>1))
+S5A_model_TN <- lm(`TOTAL NITROGEN` ~ Flow, data = S5A_WQ_and_flow)
+G538_model_TN <- lm(`TOTAL NITROGEN` ~ Flow, data = G538_WQ_and_flow)
+G538_model_greater_than_one_TN <- lm(`TOTAL NITROGEN` ~ Flow, data = filter(G538_WQ_and_flow,Flow>1))
 
-#S5A model residual plot
-ggplot(data.frame(x = seq(S5A_model$residuals), y = S5A_model$residuals)) +
-geom_point(aes(x, y)) +labs(x = "Index", y = "Residuals", title = paste("Residuals of", format(S5A_model$call)))
+#create DF of model summaries 
+#TP models
 
-#S5A model residual plot
-ggplot(data.frame(x = seq(G538_model$residuals), y = G538_model$residuals)) +
-geom_point(aes(x, y)) +labs(x = "Index", y = "Residuals", title = paste("Residuals of", format(G538_model$call)))
+df_S5A_mod <-data.frame(Name="TP S5A Linear Regression ",Data="All Observations") %>% cbind( glance(S5A_model) )
+df_S5A_mod_greater_than_one <-data.frame(Name="TP S5A Linear Regression ",Data="Flow >1") %>% cbind(glance(S5A_model_greater_than_one)) 
+df_S5A_mod_NLS_greater_than_one <-data.frame(Name="TP S5A Logistic Growth Model ",Data="Flow >1") %>% cbind(glance(S5A_model_growth )) 
+df_G538_mod <-data.frame(Name="TP G538 Linear Regression ",Data="All Observations") %>% cbind(glance(G538_model) )
+df_G538_mod_greater_than_one <-data.frame(Name="TP G538 Linear Regression ",Data="Flow >1") %>% cbind(glance(G538_model_greater_than_one) )
 
+All_mods_TP<- bind_rows(df_S5A_mod,df_S5A_mod_greater_than_one,df_S5A_mod_NLS_greater_than_one,df_G538_mod,df_G538_mod_greater_than_one) 
 
-# Time Series Plots -------------------------------------------------------
+#TN models
+df_S5A_mod_TN <-data.frame(Name="TN S5A Linear Regression ",Data="All Observations") %>% cbind( glance(S5A_model_TN) )
+df_S5A_mod_greater_than_one_TN <-data.frame(Name="TN S5A Linear Regression ",Data="Flow >1") %>% cbind(glance(S5A_model_greater_than_one_TN)) 
+df_G538_mod_TN <-data.frame(Name="TN G538 Linear Regression ",Data="All Observations") %>% cbind(glance(G538_model_TN) )
+df_G538_mod_greater_than_one_TN <-data.frame(Name="TN G538 Linear Regression ",Data="Flow >1") %>% cbind(glance(G538_model_greater_than_one_TN) )
 
-#Create combined DF of S5A and G538
-WQ_Time_Series <- mutate(select(S5A_WQ_and_flow,Date,Flow,`PHOSPHATE, TOTAL AS P`,`TOTAL NITROGEN`),Station="S5A") %>%
-rbind(mutate(select(G538_WQ_and_flow,Date,Flow,`PHOSPHATE, TOTAL AS P`,`TOTAL NITROGEN`),Station="G538") ) %>%
-mutate(Year=year(Date),Month=month(Date,label = TRUE,abbr = TRUE),yday=yday(Date),mday=mday(Date)) %>%
-mutate(datetime =make_datetime(2020, Month, mday, 12,0, 0))  %>%
-pivot_longer(names_to="Parameter",values_to="Value",3:4)
+All_mods_TN<- bind_rows(df_S5A_mod_TN,df_S5A_mod_greater_than_one_TN,df_G538_mod_TN,df_G538_mod_greater_than_one_TN) 
 
-#Seasonal WQ at G538 and S5A
-ggplot(WQ_Time_Series,aes(datetime,Value,color=as.factor(Station)))+geom_point()+theme_bw()+geom_smooth(fill="grey")+facet_wrap(~Parameter,ncol=1,scales="free_y")+
-labs(y= expression(mg~L^-1),x="Date", title = "Seasonal WQ Pattern at G538 and S5A")+
-scale_x_datetime(breaks =make_datetime(2020,1:12), labels = month.abb) 
+#combine model summaries
 
-ggsave("Seasonal WQ Pattern.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 8, units = "in", dpi = 300, limitsize = TRUE)
+All_mods <-bind_rows(All_mods_TP,All_mods_TN)
 
-#Seasonal plots
-ggplot(S5A_WQ_and_flow,aes(Date,`PHOSPHATE, TOTAL AS P`*1000))+geom_point()+theme_bw()+geom_smooth(method="loess",span=.2)+
-geom_line(data=S5A_Flow,aes(Date,Flow/10),color="red")+  scale_y_continuous( sec.axis = sec_axis(~.*10, name = "Flow (CFS)"))+
-scale_x_datetime(date_breaks = "3 months",date_labels = "%b%y")+
-labs(y= expression(TP~mu~g~L^-1),x="Flow (cfs)", title = "Total Phosphorus vs Flow at S5A from Grab Samples from Jan 2015 to August 2022")
+#plot residuals
+ggplot(augment(S5A_model,drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`)), aes(x=.fitted, y=.std.resid)) + geom_point() + geom_smooth(color="grey50",fill="grey") + xlab("Fitted Values") + ylab("Studentized Residuals")
+ggplot(augment(S5A_model_greater_than_one,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1)), aes(x=.fitted, y=.resid)) + geom_point() + geom_smooth(color="grey50",fill="grey") + xlab("Fitted Values") + ylab("Studentized Residuals")
+ggplot(augment(S5A_model_growth,drop_na(filter(S5A_WQ_and_flow,Flow>1),`PHOSPHATE, TOTAL AS P`)), aes(x=.fitted, y=.resid)) + geom_point() + geom_smooth(color="grey50",fill="grey") + xlab("Fitted Values") + ylab("Studentized Residuals")
 
-ggplot(mutate(S5A_WQ_and_flow,`Flow Category`=ifelse(Flow>0,"Flow","No Flow")),aes(month(Date,label = TRUE),`PHOSPHATE, TOTAL AS P`))+
-geom_jitter(color="light blue")+geom_boxplot(alpha=.5)+theme_bw()+facet_wrap(~`Flow Category`)
-
-
-#Create DF to plot daily average flow 
-G538_Daily_Average_flow_year_month<-G538_Daily_Average_flow %>% 
-mutate(Year=year(Day),Month=month(Day,label = TRUE,abbr = TRUE),yday=yday(Day),mday=mday(Day)) %>%
-mutate(datetime =make_datetime(2020, Month, mday, 12,0, 0))  
+#Plot fitted lines
+ggplot(augment(S5A_model,drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),se_fit=TRUE),aes(Flow,`PHOSPHATE, TOTAL AS P`))+geom_point()+
+geom_line(aes(Flow,.fitted),color="blue")+
+geom_ribbon(aes(ymin=.fitted-1.96*.se.fit, ymax=.fitted+1.96*.se.fit), alpha=0.2)+
+geom_line(data=augment(S5A_model_greater_than_one,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1)),aes(Flow,.fitted),color="red")+
+geom_ribbon(data=augment(S5A_model_greater_than_one,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1),se_fit=TRUE),aes(ymin=.fitted-1.96*.se.fit, ymax=.fitted+1.96*.se.fit), alpha=0.2)+
+geom_line(data=augment(S5A_model_growth,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1)),aes(Flow,.fitted),color="black")+
+geom_ribbon(data=augment(S5A_model_growth,filter(drop_na(S5A_WQ_and_flow,`PHOSPHATE, TOTAL AS P`),Flow>1),se_fit=TRUE),aes(ymin=.fitted-1.96*.se.fit, ymax=.fitted+1.96*.se.fit), alpha=0.2)
   
-#Seasonal flow at G538
-ggplot(G538_Daily_Average_flow_year_month,aes(datetime,`Daily Flow`,color=as.factor(Year)))+geom_point()+geom_line()+theme_bw()+
-labs(y= "Mean Daily Flow (cfs)",x="Date", title = "G538 Seasonal flow Pattern")+
-scale_x_datetime(breaks =make_datetime(2020,1:12), labels = month.abb) 
+#S5A model diagnostic plots plot
+par(mfrow=c(2,2)) #change plot format to display all 4 diagnostic plots at once
+plot(S5A_model_greater_than_one)
+plot(S5A_model)
+plot(nlsResiduals(S5A_model_growth), which = 0)  #uses NLStools to create diagnostic plots
 
-ggsave("G538 Seasonal flow Pattern.jpg", plot = last_plot(), path ="./Figures/",width = 10.666, height = 6, units = "in", dpi = 300, limitsize = TRUE)
-
-
+tidy(S5A_model)
+tidy(S5A_model_growth)
+glance(S5A_model_growth)
 
 # Test Code ---------------------------------------------------------------
 
--- EDA Report- Effects of Flow on TN and TP at G538 and S5A.R
+++ EDA Report- Effects of Flow on TN and TP at G538 and S5A.R
@@ -4,3 +4,4 @@
 
 
 rmarkdown::render("EDA Report- Effects of Flow on TN and TP at G538 and S5A.Rmd", envir=.GlobalEnv)
+
